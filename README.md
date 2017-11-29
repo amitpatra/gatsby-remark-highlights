91ec2f70e5accae2fb00a99ea4106e77ad251a1c
Amit Patra <amit.patra@msn.com>
2017-11-29 19:37:23 +0530

    Initial Commit :tada:

---
+++ README.md
@@ -0,0 +1,1 @@
+# gatsby-remark-highlights :star_struck:
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const visit = require(`unist-util-visit`);
+const Highlights = require('highlights');
+
+const highlighter = new Highlights();
+
+// Highlighing function
+const highlight = (contents, lang) => {
+	return highlighter.highlightSync({
+		fileContents: contents,
+		filePath: `fake.${lang}`
+	});
+};
+
+module.exports = ({ markdownAST }) => {
+	visit(markdownAST, `code`, node => {
+		node.type = `html`;
+		node.value = highlight(node.value, node.lang);
+	});
+};
